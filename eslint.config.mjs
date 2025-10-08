65e98337d539575ebfbae949e73f3cb95ddad5ee
Moritz Lederle <moritz.lederle@grommunio.com>
2025-10-08 07:59:48 +0200

    Add eslint deps & config

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  stylistic.configs.recommended,
+)
