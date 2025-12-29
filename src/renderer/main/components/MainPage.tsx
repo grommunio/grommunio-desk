fc60e3ed50a0c0761bce75fd0798354f7ccb1776
Moritz Lederle <moritz.lederle@grommunio.com>
2025-12-29 12:16:26 +0100

    Add MainPage option to set server

--- src/renderer/main/components/MainPage.tsx
+++ src/renderer/main/components/MainPage.tsx
@@ -1,11 +1,22 @@
 // Copyright (c) 2020-present grommunio GmbH. All Rights Reserved.
 
-import React from 'react'
+import React, { useState } from 'react'
 
 const MainPage = (): React.ReactElement => {
+  const [input, setInput] = useState('')
+
+  const onSend = (): void => {
+    console.log('New server', input)
+    window.electronAPI.saveServer(input)
+  }
+
   return (
     <div>
-      Hello World
+      <div>grommunio Server</div>
+      <input onInput={e => setInput((e.target as HTMLInputElement).value)} type="url" placeholder="https://mail.example.com" />
+      <button onClick={onSend}>
+        Send
+      </button>
     </div>
   )
 }
