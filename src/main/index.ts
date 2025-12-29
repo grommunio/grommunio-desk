b8d9cb12631a4c2f70430771b01429bc3f9fdc45
Moritz Lederle <moritz.lederle@grommunio.com>
2025-12-29 12:16:21 +0100

    Add class for MainWindow (main process)

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,17 +1,23 @@
 // Copyright (c) 2020-present grommunio GmbH. All Rights Reserved.
 
 import { app, BrowserWindow } from 'electron'
-import { createMainWindow } from './windows/main'
+
+import MainWindow from './windows/main'
+import store from './utils/store'
 
 const isProduction = process.env.NODE_ENV === 'production'
-
 console.log('Production:', isProduction)
 
-app.on('ready', () => createMainWindow(isProduction))
+let mainWindow: MainWindow | undefined
+
+app.on('ready', () => {
+  const server = store.get('server')
+  mainWindow = new MainWindow(isProduction, server)
+  mainWindow.show()
+})
 
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their
-  // menu bar to stay active until the user quits
+  // On macOS it is common for applications and their menu bar to stay active until the user quits
   // explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
@@ -19,10 +25,9 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the
-  // app when the dock icon is clicked and there are no
+  // On macOS it's common to re-create a window in the app when the dock icon is clicked and there are no
   // other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createMainWindow(isProduction)
+    mainWindow?.createWindow()
   }
 })
--- src/main/windows/main/index.ts
+++ src/main/windows/main/index.ts
@@ -1,21 +1,142 @@
 // Copyright (c) 2020-present grommunio GmbH. All Rights Reserved.
 
-import { BrowserWindow } from 'electron'
+import { BrowserWindow, shell, HandlerDetails, WindowOpenHandlerResponse, Menu } from 'electron'
+import store from '../../utils/store'
 
-export function createMainWindow(isProduction: boolean): void {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true, // TODO: research about security
-      contextIsolation: false,
-    },
-  })
+export default class MainWindow {
+  win?: BrowserWindow
+  server: string | undefined
+  isProduction: boolean
 
-  if (isProduction)
-    win.loadFile('main.html')
-  else {
-    win.loadURL('http://localhost:8080/main.html')
-    win.webContents.openDevTools()
+  constructor(isProduction: boolean, server: string | undefined) {
+    this.server = server
+    this.isProduction = isProduction
+    this.createWindow()
+  }
+
+  createWindow(): void {
+    if (this.win != null)
+      return
+
+    const windowSize = store.get('windowSize')
+    this.win = new BrowserWindow({
+      minWidth: 800,
+      minHeight: 600,
+      width: windowSize[0],
+      height: windowSize[1],
+      title: 'grommunio Desk',
+      webPreferences: {
+      },
+      show: false,
+    })
+
+    const isMac = process.platform === 'darwin'
+    const menu = Menu.buildFromTemplate([
+      {
+        label: 'File',
+        submenu: [
+          // { type: 'separator' },
+          isMac ? { role: 'close' } : { role: 'quit' },
+        ],
+      },
+      {
+        label: 'Server',
+        submenu: [
+          {
+            click: (): void => {
+              store.set('server', undefined)
+              this.reloadView(undefined)
+            },
+            label: 'Switch server',
+          },
+        ],
+      },
+      {
+        label: 'View',
+        submenu: [
+          { role: 'toggleDevTools' },
+        ],
+      },
+      {
+        label: 'Help',
+        submenu: [
+          { role: 'about' },
+        ],
+      },
+    ])
+    this.win.setMenu(menu)
+
+    this.registerWinListeners()
+    this.loadView()
+  }
+
+  private loadView(): void {
+    if (this.win == null)
+      return
+
+    this.registerViewListeners()
+
+    console.log('Server', this.server)
+
+    if (this.server != undefined) {
+      this.win.webContents.loadURL(this.server)
+    }
+    else {
+      if (this.isProduction)
+        this.win.webContents.loadFile('main.html')
+      else
+        this.win.webContents.loadURL('http://localhost:8080/main.html')
+    }
+
+    if (!this.isProduction)
+      this.win.webContents.openDevTools()
+  }
+
+  reloadView(server: string | undefined): void {
+    this.server = server
+    this.loadView()
+  }
+
+  show(): void {
+    if (this.win == null) {
+      console.error('Variable \'win\' is null / undefined')
+      return
+    }
+    this.win.show()
+  }
+
+  private registerWinListeners(): void {
+    if (this.win == null) {
+      console.error('Variable \'win\' is unexpectedly null / undefined')
+      return
+    }
+    this.win.on('close', () => {
+      if (this.win == null)
+        return
+      store.set('windowSize', this.win.getSize())
+      this.win = undefined
+    })
+  }
+
+  private registerViewListeners(): void {
+    if (this.win == null) {
+      console.error('Variable \'win\' is unexpectedly null / undefined')
+      return
+    }
+
+    this.win.webContents.on('page-title-updated', (e) => {
+      e.preventDefault()
+    })
+
+    this.win.webContents.on('will-prevent-unload', (e) => {
+      e.preventDefault()
+    })
+
+    this.win.webContents.setWindowOpenHandler(({ url }: HandlerDetails): WindowOpenHandlerResponse => {
+      shell.openExternal(url)
+      return { action: 'deny' }
+    })
+
+    // TODO: prevent redirects to external pages
   }
 }
