d72b14b935764a763c5d3ccb044892dc205f0994
Moritz Lederle <moritz.lederle@grommunio.com>
2026-04-18 15:24:14 +0200

    Improve confirmDialog types
    Define the buttons for each ConfirmDialogType beforehand.

--- src/types/dialog/confirmDialog.ts
+++ src/types/dialog/confirmDialog.ts
@@ -4,17 +4,29 @@ import { InterpolationOptions } from 'i18next'
 
 import { UserDialogButtonTemplate, UserTextDialogTemplate, UserButtonDialogTemplate } from './templates'
 import { UserDialogCancelButton } from './buttons'
+import { ValidateDialogArgs } from './misc'
 import { Server } from '../misc'
 
 type UserConfirmDialogReturnToStartPageButton = UserDialogButtonTemplate<'confirm.returnToStartPage'>
 type UserConfirmDialogRemoveServerButton = UserDialogButtonTemplate<'confirm.removeServer', { server: Server }>
 export type UserConfirmDialogButton = UserDialogCancelButton | UserConfirmDialogReturnToStartPageButton | UserConfirmDialogRemoveServerButton
 
-interface UserConfirmDialogTextArgs {
-  'confirm.loadFailed': { url: string, interpolation?: InterpolationOptions }
-  'confirm.removeServer': { server: Server }
+interface UserConfirmDialogArgsEntry {
+  textArgs: Record<string, unknown> | undefined
+  buttons: UserConfirmDialogButton[]
 }
-type UserConfirmDialogGeneric<Text extends keyof UserConfirmDialogTextArgs>
-  = UserTextDialogTemplate<'confirm', Text, Text, UserConfirmDialogTextArgs[Text]>
-    & UserButtonDialogTemplate<'confirm', Text, UserConfirmDialogButton[]>
-export type UserConfirmDialog = { [K in keyof UserConfirmDialogTextArgs]: UserConfirmDialogGeneric<K> }[keyof UserConfirmDialogTextArgs]
+type UserConfirmDialogArgs = ValidateDialogArgs<UserConfirmDialogArgsEntry, {
+  'confirm.loadFailed': {
+    textArgs: { url: string, interpolation?: InterpolationOptions }
+    buttons: [{ type: 'confirm.returnToStartPage', triggerOnEnter: true }]
+  }
+  'confirm.removeServer': {
+    textArgs: { server: Server }
+    buttons: [{ type: 'cancel' }, { type: 'confirm.removeServer', callbackParams: { server: Server }, triggerOnEnter: true }]
+  }
+}>
+
+type UserConfirmDialogGeneric<ConfirmDialogType extends Extract<keyof UserConfirmDialogArgs, string>>
+  = UserTextDialogTemplate<'confirm', ConfirmDialogType, ConfirmDialogType, UserConfirmDialogArgs[ConfirmDialogType]['textArgs']>
+    & UserButtonDialogTemplate<'confirm', ConfirmDialogType, UserConfirmDialogArgs[ConfirmDialogType]['buttons']>
+export type UserConfirmDialog = { [K in keyof UserConfirmDialogArgs]: UserConfirmDialogGeneric<K> }[keyof UserConfirmDialogArgs]
---
+++ src/types/dialog/misc.ts
@@ -0,0 +1,3 @@
+// Copyright (c) 2020-2026 grommunio GmbH. All Rights Reserved.
+
+export type ValidateDialogArgs<K extends object, T extends Record<string, K>> = T
