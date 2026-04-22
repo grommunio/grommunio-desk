e32bb1fe8d07442f62d220b7d67e1582b4e180fc
Moritz Lederle <moritz.lederle@grommunio.com>
2026-04-22 16:41:10 +0200

    Improve SelectField component
    * add className parameter
    * add auto-scroll functionality when using arrow keys
    * add remember-scroll-position functionality
    * fix bug (when leaving the field without selecting an item,
      highlightedIndex is not reset correctly)

--- src/renderer/components/selectField.tsx
+++ src/renderer/components/selectField.tsx
@@ -1,40 +1,53 @@
 // Copyright (c) 2020-2026 grommunio GmbH. All Rights Reserved.
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 import styles from './selectField.module.css'
 import componentStyles from './component.module.css'
 import { SelectFieldOption } from '../../types/selectField'
 
 interface Props<T> {
+  className?: string
   options: SelectFieldOption<T>[]
   placeholder?: string
   placeholderOption?: SelectFieldOption<T>
-  onChange?: (value: T) => void
+  onChange: (value: T) => void
 }
 
 // TODO: use consistently function syntax instead of arrow systax
-// TODO: scroll when using arrow keys
-// TODO: auto scroll to the last selected value
 function SelectField<T>(props: Props<T>): React.ReactElement {
   const [isOpen, setOpen] = useState(false)
   const [selectedOption, setSelectedOption] = useState<SelectFieldOption<T> | null>(null)
   const [highlightedIndex, setHighlightedIndex] = useState(0)
+  const selectDisplayRef = useRef<HTMLDivElement>(null)
+  const selectOptionsRef = useRef<Record<number, HTMLLIElement | null>>({})
 
   useEffect(() => {
     if (props.placeholderOption != null) {
       setSelectedOption(props.placeholderOption)
       setHighlightedIndex(props.options.findIndex(val => val === props.placeholderOption))
+      props.onChange(props.placeholderOption.value)
     }
-  }, [props.placeholderOption, props.options])
+  }, [])
 
-  const handleOptionClick = (option: SelectFieldOption<T>): void => {
+  const handleOptionClick = (option: SelectFieldOption<T>, inputType: 'keyboard' | 'mouse'): void => {
     setSelectedOption(option)
+    props.onChange(option.value)
     setOpen(false)
-    props.onChange?.(option.value)
+    if (inputType === 'mouse')
+      selectDisplayRef.current?.blur()
   }
 
+  useEffect(() => {
+    if (isOpen && selectedOption != null) {
+      const index = props.options.indexOf(selectedOption)
+      setHighlightedIndex(index)
+      selectOptionsRef.current[index]?.scrollIntoView({ block: 'center' })
+    }
+  }, [isOpen])
+
   const handleKeyDown = (e: React.KeyboardEvent): void => {
+    e.stopPropagation()
     if (!isOpen && e.key === 'Enter') {
       setOpen(true)
       return
@@ -42,13 +55,21 @@ function SelectField<T>(props: Props<T>): React.ReactElement {
 
     switch (e.key) {
       case 'ArrowDown':
-        setHighlightedIndex(prev => prev < props.options.length - 1 ? prev + 1 : prev)
+        setHighlightedIndex((prev) => {
+          const next = prev < props.options.length - 1 ? prev + 1 : prev
+          selectOptionsRef.current[next]?.scrollIntoView({ block: 'nearest' })
+          return next
+        })
         break
       case 'ArrowUp':
-        setHighlightedIndex(prev => (prev > 0 ? prev - 1 : prev))
+        setHighlightedIndex((prev) => {
+          const next = prev > 0 ? prev - 1 : prev
+          selectOptionsRef.current[next]?.scrollIntoView({ block: 'nearest' })
+          return next
+        })
         break
       case 'Enter':
-        handleOptionClick(props.options[highlightedIndex])
+        handleOptionClick(props.options[highlightedIndex], 'keyboard')
         break
       case 'Escape':
         setOpen(false)
@@ -58,13 +79,14 @@ function SelectField<T>(props: Props<T>): React.ReactElement {
 
   return (
     <div
-      className={styles.field}
+      className={`${styles.field} ${props.className != null ? props.className : ''}`}
       onKeyDown={handleKeyDown}
     >
       <div
         className={`${componentStyles.component} ${styles.display} ${selectedOption ? styles.selected : ''}`}
         onClick={() => setOpen(prev => !prev)}
         onBlur={() => setOpen(false)}
+        ref={selectDisplayRef}
         tabIndex={0}
       >
         {selectedOption ? selectedOption.label : props.placeholder}
@@ -80,7 +102,8 @@ function SelectField<T>(props: Props<T>): React.ReactElement {
                   className={`${styles.option} ${index === highlightedIndex ? styles.highlighted : ''}`}
                   onMouseMove={() => setHighlightedIndex(index)}
                   onMouseDown={e => e.preventDefault()} // prevent focus change
-                  onClick={() => handleOptionClick(option)}
+                  onClick={() => handleOptionClick(option, 'mouse')}
+                  ref={(ref) => { selectOptionsRef.current[index] = ref }}
                 >
                   {option.label}
                 </li>
